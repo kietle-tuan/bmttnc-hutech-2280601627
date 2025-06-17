53950eeb41d2edf32865a7da57ef15eff6458efa
kietle-tuan <jeontuankiet@gmail.com>
2025-06-17 08:50:16 +0700

    Delete LAB-01 directory

--- LAB-01/LAB-01/ex02/ex02/03.py
+++
@@ -1,5 +0,0 @@
-so = int(input("Nhaajp moojt soos nguyeen: "))
-if so % 2 == 0:
-    print(so, "là số chẵn.")
-else:
-    print(so, "không phải là số chẵn.")
--- LAB-01/LAB-01/ex02/ex02/06.py
+++
@@ -1,4 +0,0 @@
-input_str = input("Nhập X, Y: ")
-dimensions= [int(x) for x in input_str.split(',')]
-rowNum=dimensions[0]
-colNum=dimensions[1]
